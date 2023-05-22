e388e1dd95969ef003c8893200289452aa2a3c26
beymax0605 <beymax0605@gmail.com>
2023-05-22 10:19:48 +0900

    complete

---
+++ .history/config_20230521004554.py
@@ -0,0 +1,5 @@
+token = "MTEwOTUwMjQwODM4NTYzNDM4Nw.GLu2ak.VZAKlq6smNgIjBtMdEgo3Gi3YV1uDf1A5QYDes" #if token is here, the email/password are ignored
+daEmail = "exampleEmail@gmail.com" #put discord email here
+daPassword = "examplePassword1234" #put discord password here
+def getCreds():
+    return(token, daEmail,daPassword)
---
+++ config.py
@@ -0,0 +1,5 @@
+token = "MTEwOTUwMjQwODM4NTYzNDM4Nw.GLu2ak.VZAKlq6smNgIjBtMdEgo3Gi3YV1uDf1A5QYDes" #if token is here, the email/password are ignored
+daEmail = "beymax0605@gmail.com" #put discord email here
+daPassword = "ssh199672" #put discord password here
+def getCreds():
+    return(token, daEmail,daPassword)
